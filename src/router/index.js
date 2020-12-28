1fd790ff519d60af8cc71f4a07d789258afee818
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-12-28 10:30:41 +0630

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Testing_api from '../views/Testing_api.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/testing_api',
+    name: 'Testing_api',
+    component: Testing_api
   },
   {
     path: '/about',
